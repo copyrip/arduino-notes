822f807ef2ab85ef9bec6ee934eb0980f27906a3
copyrip <martin@copyright.rip>
2024-02-23 14:50:02 +0100

    STA + Oled yay !

---
+++ README.md
@@ -0,0 +1,4 @@
+# arduino note book
+
+## oled + ESP8266
+* a simple oled sketch to update text displayed on a screen
---
+++ esp_form_sta_oled/esp_form_sta_oled.ino
@@ -0,0 +1,297 @@
+/*********
+ * OLED WEBSERVER
+*********/
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <Hash.h>
+#include <FS.h>
+#include <ESPAsyncWebServer.h>
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// SCL GPIO5
+// SDA GPIO4
+#define OLED_RESET 0  // GPIO0
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+#if (SSD1306_LCDHEIGHT != 48)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+AsyncWebServer server(80);
+
+// REPLACE WITH YOUR NETWORK CREDENTIALS
+const char* ssid = "algo";
+const char* password = "19051905";
+
+const char* PARAM_STRING = "inputString";
+const char* PARAM_INT = "inputInt";
+const char* PARAM_FLOAT = "inputFloat";
+
+// HTML web page to handle 3 input fields (inputString, inputInt, inputFloat)
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>ESP Input Form</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script>
+    function submitMessage() {
+      //alert("Saved value to ESP SPIFFS");
+      setTimeout(function(){ document.location.reload(false); }, 500);   
+    }
+  </script></head><body>
+  <!--
+  <form action="/get" target="hidden-form">
+    inputString (current value %inputString%): <input type="textarea" name="inputString">
+    <input type="submit" value="Submit" onclick="submitMessage()">
+  </form><br>
+  -->
+  <form action="/get" target="hidden-form">
+    inputString (current value %inputString%): <textarea type="textarea" name="inputString"></textarea>
+    <input type="submit" value="Submit" onclick="submitMessage()">
+  </form><br>
+  <form action="/get" target="hidden-form">
+    inputInt (current value %inputInt%): <input type="number " name="inputInt">
+    <input type="submit" value="Submit" onclick="submitMessage()">
+  </form><br>
+  <form action="/get" target="hidden-form">
+    inputFloat (current value %inputFloat%): <input type="number " name="inputFloat">
+    <input type="submit" value="Submit" onclick="submitMessage()">
+  </form>
+  <iframe style="display:none" name="hidden-form"></iframe>
+</body></html>)rawliteral";
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
+
+String readFile(fs::FS &fs, const char * path){
+  Serial.printf("Reading file: %s\r\n", path);
+  File file = fs.open(path, "r");
+  if(!file || file.isDirectory()){
+    Serial.println("- empty file or failed to open file");
+    return String();
+  }
+  Serial.println("- read from file:");
+  String fileContent;
+  while(file.available()){
+    fileContent+=String((char)file.read());
+  }
+  file.close();
+  Serial.println(fileContent);
+  return fileContent;
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+  Serial.printf("Writing file: %s\r\n", path);
+  File file = fs.open(path, "w");
+  if(!file){
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  if(file.print(message)){
+    Serial.println("- file written");
+  } else {
+    Serial.println("- write failed");
+  }
+  file.close();
+}
+
+// Replaces placeholder with stored values
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "inputString"){
+    return readFile(SPIFFS, "/inputString.txt");
+  }
+  else if(var == "inputInt"){
+    return readFile(SPIFFS, "/inputInt.txt");
+  }
+  else if(var == "inputFloat"){
+    return readFile(SPIFFS, "/inputFloat.txt");
+  }
+  return String();
+}
+
+void setup() {
+  Serial.begin(115200);
+  // Initialize SPIFFS
+  #ifdef ESP32
+    if(!SPIFFS.begin(true)){
+      Serial.println("An Error has occurred while mounting SPIFFS");
+      return;
+    }
+  #else
+    if(!SPIFFS.begin()){
+      Serial.println("An Error has occurred while mounting SPIFFS");
+      return;
+    }
+  #endif
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Failed!");
+    return;
+  }
+  Serial.println();
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Send web page with input fields to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", index_html, processor);
+  });
+
+  // Send a GET request to <ESP_IP>/get?inputString=<inputMessage>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    // GET inputString value on <ESP_IP>/get?inputString=<inputMessage>
+    if (request->hasParam(PARAM_STRING)) {
+      inputMessage = request->getParam(PARAM_STRING)->value();
+      writeFile(SPIFFS, "/inputString.txt", inputMessage.c_str());
+    }
+    // GET inputInt value on <ESP_IP>/get?inputInt=<inputMessage>
+    else if (request->hasParam(PARAM_INT)) {
+      inputMessage = request->getParam(PARAM_INT)->value();
+      writeFile(SPIFFS, "/inputInt.txt", inputMessage.c_str());
+    }
+    // GET inputFloat value on <ESP_IP>/get?inputFloat=<inputMessage>
+    else if (request->hasParam(PARAM_FLOAT)) {
+      inputMessage = request->getParam(PARAM_FLOAT)->value();
+      writeFile(SPIFFS, "/inputFloat.txt", inputMessage.c_str());
+    }
+    else {
+      inputMessage = "No message sent";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/text", inputMessage);
+  });
+  server.onNotFound(notFound);
+  server.begin();
+
+  Serial.begin(9600);
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
+  // init done
+
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+ // display.display();
+ // delay(2000);
+
+  // Clear the buffer.
+  display.clearDisplay();
+/*
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello, world!");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  */
+}
+
+void loop() {
+  
+  // To access your stored values on inputString, inputInt, inputFloat
+  String yourInputString = readFile(SPIFFS, "/inputString.txt");
+  Serial.print("*** Your inputString: ");
+  Serial.println(yourInputString);
+  
+  int yourInputInt = readFile(SPIFFS, "/inputInt.txt").toInt();
+  Serial.print("*** Your inputInt: ");
+  Serial.println(yourInputInt);
+  
+  float yourInputFloat = readFile(SPIFFS, "/inputFloat.txt").toFloat();
+  Serial.print("*** Your inputFloat: ");
+  Serial.println(yourInputFloat);
+  delay(100);
+
+  yourInputString ? displayText(yourInputString) : displayText("...");
+  delay(200);
+  display.clearDisplay();
+  
+  dice();
+  delay(200);
+  display.clearDisplay();
+/*
+  display.setTextSize(4);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("13");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+
+  
+
+  display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.println(3.141592);
+
+  /*
+  testdrawcircle();
+  delay(1000);
+  display.clearDisplay();
+  */
+  Serial.print("=== END LOOP ===");
+ 
+}
+void dice(void){
+  int num = random(1,21);
+  display.setTextSize(5);
+  display.setCursor(0,0);
+  display.setTextColor(WHITE);
+  display.println(num);
+  display.display();
+}
+
+void displayText(String text){
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.clearDisplay();
+  display.println(text);
+  display.display();
+}
+
+void testdrawcircle(void) {
+  for (int16_t i=0; i<display.height(); i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
+    display.display();
+    delay(1);
+  }
+}
