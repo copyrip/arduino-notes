5069f6e21fd0abeb879faef00d8ff66423e63a0f
copyrip <martin@copyright.rip>
2024-03-12 18:27:23 +0100

    lightwork

--- esp_form_sta_oled/esp_form_sta_oled.ino
+++ esp_form_sta_oled/esp_form_sta_oled.ino
@@ -25,36 +25,6 @@
 #define OLED_RESET 0  // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-
-#define LOGO16_GLCD_HEIGHT 16
-#define LOGO16_GLCD_WIDTH  16
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
-
-#if (SSD1306_LCDHEIGHT != 48)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
-
 AsyncWebServer server(80);
 
 // REPLACE WITH YOUR NETWORK CREDENTIALS
@@ -231,6 +201,9 @@ void setup() {
 }
 
 void loop() {
+  Serial.println();
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
   
   // To access your stored values on inputString, inputInt, inputFloat
   String yourInputString = readFile(SPIFFS, "/inputString.txt");
@@ -277,10 +250,10 @@ void loop() {
  
 }
 void dice(void){
-  display.setFont(&FreeSerif12pt7b);
+  //display.setFont(&FreeSerif12pt7b);
   int num = random(1,21);
   //display.setTextSize(5);
-  display.setCursor(0,0);
+  display.setCursor(10,10);
   display.setTextColor(WHITE);
   display.println(num);
   display.display();
